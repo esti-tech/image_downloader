5421b50e5ec5de0ee0612a47c35d740117bdcff4
estifanos <tekle.estifanos.17@gmail.com>
2024-06-22 07:39:53 -0700

    readme updated

--- readme.md
+++ readme.md
@@ -5,5 +5,5 @@ A simple Python package to download images from a URL.
 ## Installation
 
 ```sh
-pip install git+https://github.com/yourusername/image_downloader.git
+pip install git+https://github.com/esti-tech/image_downloader.git
 ```
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description='A simple package to download images from a URL.',
     author='Estifanos Abebaw',
     author_email='tekl.estifano.17@gmail.com',
-    url='https://github.com/esti_tech/image_downloader',  # replace with your repository URL
+    url='https://github.com/esti-tech/image_downloader.git',  
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
